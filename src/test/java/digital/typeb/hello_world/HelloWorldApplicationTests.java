e332e76dac4e7f56a1445e67a5b88775f6f49d1a
NimnaKs <nimnakaveesha123456@gmail.com>
2025-11-06 21:02:30 +0530

    feat: initialize TypeB HelloWorld Spring Boot project (Java 17, Web, Test)

---
+++ src/test/java/digital/typeb/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.typeb.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
